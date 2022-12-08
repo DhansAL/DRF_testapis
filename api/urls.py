a5aef901131ede158fb2b89fa2f9b3d5a4ba609e
Dhans <sgdhans@gmail.com>
2022-12-08 17:13:29 +0530

    basic DRF server setup

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from .views import ItemList,ItemDetail,LocationList,LocationDetail
+from django.urls import path
+
+
+urlpatterns = [
+ path("items/",ItemList.as_view()),   
+ path("item/<int:pk>/",ItemDetail.as_view()),   
+ path("location/",LocationList.as_view()),   
+ path("location/<int:pk>/",LocationDetail.as_view()),   
+  ]
